69e04d878f5265820165704c0083b30ae00e22af
Josh Borup <joshborup@Joshs-MacBook-Pro.local>
2019-03-22 14:26:38 -0700

    initial commit

---
+++ christmas.js
@@ -0,0 +1,14 @@
+const christmas = {
+  day1: "josh",
+  day2: "sdaff",
+  day3: "dgdfs",
+  day4: "sdfg",
+  day5: "sdfg",
+  day6: "sdfgdfg",
+  day7: "",
+  day8: "",
+  day9: "",
+  day10: "",
+  day11: "",
+  day12: ""
+};
